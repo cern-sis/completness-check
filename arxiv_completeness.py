53d53e7b6d887e35be60b5fc1b0de01dc051cf82
DonHaul <ramiro.animus@gmail.com>
2024-09-11 16:21:04 +0200

    arxiv check: add daily job
    * ref cern-sis/issues-inspire/issues/573

--- arxiv_completeness.py
+++ arxiv_completeness.py
@@ -1,6 +1,7 @@
 from datetime import date, datetime, timedelta
 
 from errbot import BotPlugin, arg_botcmd
+from errcron import CrontabMixin
 
 from lib import arxiv_completness_check_script
 
@@ -11,11 +12,19 @@ else:
     DEFAULT_FROM_DATE = TODAY - timedelta(days=1)
 
 
-class ArxivCompleteness(BotPlugin):
+def get_default_from_date():
+    if TODAY.weekday() == 0:
+        return TODAY - timedelta(days=3)
+    return TODAY - timedelta(days=1)
+
+
+class ArxivCompleteness(CrontabMixin, BotPlugin):
     """
     Arxiv Completeness Errbot Plugin
 
     """
+
+    CRONTAB = [" 0 10 * * * .daily_check"]
 
     @arg_botcmd("--from-date", dest="from_date", type=str, default=None)
     @arg_botcmd("--to-date", dest="to_date", type=str, default=None)
@@ -26,11 +35,27 @@ class ArxivCompleteness(BotPlugin):
         if from_date is not None:
             from_date = datetime.strptime(from_date, "%d-%m-%Y").date()
         else:
-            from_date = DEFAULT_FROM_DATE
+            from_date = get_default_from_date()
         if to_date is not None:
             to_date = datetime.strptime(to_date, "%d-%m-%Y").date()
         else:
-            to_date = TODAY
+            to_date = date.today()
 
         yield "Arxiv Completeness Check may take some time, please be patient"
         yield arxiv_completness_check_script.completeness_check(from_date, to_date)
+
+    def daily_check(self, polled_time):
+        client = self._bot.client
+
+        message = arxiv_completness_check_script.completeness_check(
+            get_default_from_date(), date.today()
+        )
+
+        client.send_message(
+            {
+                "type": "stream",
+                "to": "inspire",
+                "topic": "harvest",
+                "content": message,
+            }
+        )
